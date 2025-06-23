6a9bcf42c95d805cab35755ec65338c00230c48a
Darrell Moreno <dmoreno1987@outlook.com>
2025-06-23 14:38:03 -0400

    Created 3x3 grid. Added (ButtonList.jsx) add/remove row/column buttons at bottom of screen. Added Grid maker title.

---
+++ src/ButtonList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const ButtonList = () => {
+    return (
+        <div>
+            <button>Add Row</button>
+            <button>Remove Row</button>
+            <button>Add Column</button>
+            <button>Remove Column</button>
+        </div>
+    )
+}
+
+export default ButtonList;
--- src/app.jsx
+++ src/app.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
+import ButtonList from "./ButtonList";
 
 const App = () => {
   return (
-    <div className="grid">
-      <table>
-        <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table> { }
+    <div classname="app">
+      <h1>Grid Maker</h1>
+      <div className="grid">
+        <table>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table> {}
+      </div>
+      <ButtonList />
     </div>
   );
 };
